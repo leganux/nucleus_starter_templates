8c3f20de84a8145825414c192d94a90aa5b33802
Angel Erick Cruz Olivera <leganux@hotmail.com>
2024-03-31 13:34:17 -0600

    new template

---
+++ nucleus_model_FULL_Modules_SQL_generator/structure/modules/___model_name_file___/___model_name_file___.model.js
@@ -0,0 +1,25 @@
+let {Sequelize, DataTypes, sequelize} = require('./../../config/connection')
+
+const {UUIDV4} = require('sequelize');
+const moment = require('moment');
+
+
+const {{_model_name_variable_}} = sequelize.define('{{_model_name_variable_}}', {
+    _id: {
+        type: DataTypes.UUID,
+        defaultValue: UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+        field: '_id',
+    },
+    {$_field_generator_$}
+}, {
+    tableName: '{{_model_name_variable_}}s',
+    timestamps: true,
+});
+
+
+{{_model_name_variable_}}.sync({alter: true});
+
+
+module.exports = {{_model_name_variable_}};
